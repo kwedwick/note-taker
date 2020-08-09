2a97847e9ffb8654bf8c8c357ae7c7c916fdd580
KWedwick <64446581+kwedwick@users.noreply.github.com>
2020-08-08 23:04:35 -0500

    restructured folders/files

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const PORT = process.env.PORT || 3001;
+const app = express();
+const fs = require('fs');
+const path = require('path');
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './/public/index.html'))
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
