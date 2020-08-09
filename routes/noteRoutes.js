acb9902f65df33d89106c5c4a4bf73ecbb16c995
KWedwick <64446581+kwedwick@users.noreply.github.com>
2020-08-09 14:02:56 -0500

    completed API paths

---
+++ routes/noteRoutes.js
@@ -0,0 +1,57 @@
+const fs = require('fs');
+const path = require('path');
+const { notes } = require('../db/db.json')
+//const { notStrictEqual } = require('assert');
+
+function validateNote(note) {
+    if (!note.title || typeof note.title !== 'string') {
+        return false;
+    }
+    if (!note.text || typeof note.text !== 'string') {
+        return false;
+    }
+    if (note.id === notes.id) {
+        return false;
+    }
+    return true
+};
+
+function createNewNote(body, notesArray) {
+    const note = body;
+    notesArray.push(note);
+    fs.writeFileSync(
+        path.join(__dirname, '../db/db.json'),
+        JSON.stringify({ notes: notesArray })
+    )
+    return note;
+};
+
+function deleteNote(body, notesArray) {
+    const targetNote = body[0];
+    console.log(notesArray)
+    console.log(targetNote)
+
+    notesArray = notesArray.filter(note => {
+        console.log(targetNote.id + "vs" + note.id)
+        return targetNote.id !== note.id
+    })
+
+    console.log(notesArray)
+    fs.writeFileSync(
+        path.join(__dirname, '../db/db.json'),
+        JSON.stringify({ notes: notesArray })
+    )
+};
+
+
+function editNote(body, notesArray) {
+    const targetNote = body
+
+}
+
+module.exports = {
+    validateNote,
+    createNewNote,
+    deleteNote,
+    editNote
+}
--- server.js
+++ server.js
@@ -3,14 +3,64 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 const fs = require('fs');
 const path = require('path');
+//const { type } = require('os');
+const { notes } = require('./db/db.json')
+const { validateNote, createNewNote, deleteNote, editNote } = require('./routes/noteRoutes');
+const nodemon = require('nodemon');
+
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+
 
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './/public/index.html'))
+    res.sendFile(path.join(__dirname, './public/index.html'))
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('/api/notes', (req, res) => {
+    //console.log(notes)
+    res.json(notes);
+});
+app.get('/api/note/:id', (req, res) => {
+    console.log(req.body)
+    res.json(req.body)
 })
 
+app.post('/api/notes', (req, res) => {
+    // determine id
+    req.body.id = notes.length+1;
+    // check to see if the object is valid 
+    if (!validateNote(req.body)) {// if not valid, reject
+        res.status(400).send("The note is not properly filled out");
+        //console.log(req.body)
+    } else {//else post to db 
+        const note = createNewNote(req.body, notes);
+        res.json(note);
+    }
+});
 
+app.post('/api/notes/:id', (req, res) => {
 
+})
+
+app.delete('/api/notes/:id', (req, res) => {
+    const target = notes.filter(note => note.id === parseInt(req.params.id));
+
+    if (target) {
+        deleteNote(target, notes);
+        //console.log(deleteNote);
+        //res.json({ msg: 'Note Deleted' })
+        res.sendFile(path.join(__dirname, './public/notes.html'));
+    } else {
+        res.status(400).json({ msg: `No note with that id found` });
+    }
+})
 
 
 
